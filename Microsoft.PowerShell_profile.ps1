b59eeb3be507bf7ea4b297e58cd2e35689cfe75b
jayzone91 <register@j4yz0n3.de>
2023-12-23 09:48:25 +0100

    init

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,40 @@
+# region Functions
+function sudo {
+	Start-Process @args -Verb RunAs -Wait
+}
+
+function .. {
+	Set-Location ..
+}
+
+function ... {
+	Set-Location ../..
+}
+
+function .... {
+	Set-Location ../../..
+}
+
+# endregion
+
+#region aliases
+################################################################################
+# Set common aliases                                                           #
+################################################################################
+Set-Alias -Name ll -Value Get-ChildItemColor -Scope Global -Option AllScope
+Set-Alias -Name ls -Value Get-ChildItemColorFormatWide -Scope Global -Option AllScope
+Set-Alias -Name History -Value Open-HistoryFile -Scope Global -Option AllScope
+Set-Alias -Name vim -Value nvim
+
+# endregion
+
+#region execution
+################################################################################
+# Update the console title with current PowerShell elevation and version       #
+################################################################################
+Import-Module -Name Terminal-Icons
+$Host.UI.RawUI.WindowTitle = "PS | $((Invoke-WebRequest wttr.in/kassel?format="%c%t" -UseBasicParsing).content)"
+
+# endregion
+
+Invoke-Expression (&starship init powershell)
